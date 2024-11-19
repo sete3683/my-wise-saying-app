9b1dac7aab2e68cdbd5a4d645d0c333bc9087a1b
sete3683 <sete3683@gmail.com>
2024-11-19 16:29:45 +0900

    3, 4, 5단계 : 번호 및 목록 출력 기능 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,60 @@
 package org.example;
 
+import java.util.*;
+
 public class App {
+    private final static String EXIT = "종료";
+    private final static String ADD = "등록";
+    private final static String PRINT = "목록";
+
+    private Scanner scanner;
+    private Deque<Saying> sayings;
+    private int index;
+
+    private void add() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        sayings.add(new Saying(index, content, author));
+        System.out.printf("%d번 명언이 등록되었습니다.\n", index++);
+    }
+
+    private void print() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("------------------");
+
+        Iterator<Saying> iterator = sayings.descendingIterator();
+        while (iterator.hasNext()) {
+            Saying saying = iterator.next();
+            System.out.printf("%s / %s / %s\n",
+                    saying.getIndex(),
+                    saying.getAuthor(),
+                    saying.getContent());
+        }
+    }
+
+    public void run() {
+        System.out.println("==명언 앱==");
+        scanner = new Scanner(System.in);
+        sayings = new ArrayDeque<>();
+        index = 1;
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals(EXIT)) {
+                break;
+            } else if (command.equals(ADD)) {
+                add();
+            } else if (command.equals(PRINT)) {
+                print();
+            }
+        }
+
+        scanner.close();
+    }
 }
--- src/main/java/org/example/Saying.java
+++ src/main/java/org/example/Saying.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Saying {
+    private int index;
+    private String content;
+    private String author;
+
+    public Saying(int index, String content, String author) {
+        this.index = index;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getIndex() { return index; }
+    public String getContent() { return content; }
+    public String getAuthor() { return author; }
 }
